22f3627eabc46683da6b1c2c7be299c93b19aa4e
Andras Lasso <lasso@queensu.ca>
2017-04-20 13:56:24 -0400

    Moved parameter set database to external file

--- Elastix/Elastix.py
+++ Elastix/Elastix.py
@@ -110,13 +110,8 @@ class ElastixWidget(ScriptedLoadableModuleWidget):
     inputParametersFormLayout.addRow("Moving volume: ", self.movingVolumeSelector)
 
     self.registrationPresetSelector = qt.QComboBox()
-    for preset in RegistrationPresets:
+    for preset in self.logic.getRegistrationPresets():
       self.registrationPresetSelector.addItem("{0} ({1})".format(preset[RegistrationPresets_Modality], preset[RegistrationPresets_Content]))
-    ##Read default settings from application settings
-    # settings = qt.QSettings()
-    # displayConfiguration = settings.value('SlicerHeart/DisplayConfiguration', SMALL_SCREEN)
-    # displayConfigurationIndex = self.registrationPresetSelector.findData(displayConfiguration)
-    # self.registrationPresetSelector.setCurrentIndex(displayConfigurationIndex) 
     inputParametersFormLayout.addRow("Preset: ", self.registrationPresetSelector)
     
     #
@@ -263,7 +258,7 @@ class ElastixWidget(ScriptedLoadableModuleWidget):
       self.logic.deleteTemporaryFiles = not self.keepTemporaryFilesCheckBox.checked
       self.logic.logStandardOutput = self.showDetailedLogDuringExecutionCheckBox.checked
 
-      parameterFilenames = RegistrationPresets[self.registrationPresetSelector.currentIndex][RegistrationPresets_ParameterFilenames]
+      parameterFilenames = self.logic.getRegistrationPresets()[self.registrationPresetSelector.currentIndex][RegistrationPresets_ParameterFilenames]
 
       self.logic.registerVolumes(self.fixedVolumeSelector.currentNode(), self.movingVolumeSelector.currentNode(),
         parameterFilenames, self.outputTransformSelector.currentNode(), self.outputVolumeSelector.currentNode())
@@ -274,6 +269,7 @@ class ElastixWidget(ScriptedLoadableModuleWidget):
       traceback.print_exc()
     finally:
       slicer.app.restoreOverrideCursor()
+      self.registrationInProgress = False
       self.onSelect() # restores default Apply button state
 
   def addLog(self, text):
@@ -302,6 +298,7 @@ class ElastixLogic(ScriptedLoadableModuleLogic):
     self.abortRequested = False
     self.deleteTemporaryFiles = True
     self.logStandardOutput = False
+    self.registrationPresets = None
     self.customElastixBinDirSettingsKey = 'Elastix/CustomElastixPath'
     import os
     self.scriptPath = os.path.dirname(os.path.abspath(__file__))
@@ -374,11 +371,28 @@ class ElastixLogic(ScriptedLoadableModuleLogic):
     return elastixEnv
   
   def getRegistrationPresets(self):
-    presets = []
-    presets.append({ 'id': 'par000', 'name': 'Default', 'modality': 'all', 'content': 'all', 'parameterFiles': ['Par0000affine.txt', 'Par0000bspline.txt'] })
-    presets.append({ 'id': 'par001', 'name': 'Brain MR T1', 'modality': '3D MR T1, monomodal', 'content': 'brain', 'parameterFiles': ['Par0001affine.txt', 'Par0001bspline.txt']})
-    presets.append({ 'id': 'par002', 'name': 'Prostate MR', 'modality': '3D MR BFFE, monomodal', 'content': 'prostate', 'parameterFiles': ['Par0002affine.txt', 'Par0002bspline.txt']})
-    return presets
+    if self.registrationPresets:
+      return self.registrationPresets
+
+    # Read database from XML file
+    elastixParameterSetDatabasePath = os.path.join(self.scriptPath, 'Resources', 'RegistrationParameters', 'ElastixParameterSetDatabase.xml')
+    if not os.path.isfile(elastixParameterSetDatabasePath):
+      raise ValueError("Failed to open parameter set database: "+elastixParameterSetDatabasePath)
+    elastixParameterSetDatabaseXml = vtk.vtkXMLUtilities.ReadElementFromFile(elastixParameterSetDatabasePath)
+    elastixParameterSetDatabaseXml.UnRegister(None)
+    
+    # Create python list from XML for convenience
+    self.registrationPresets = []
+    for parameterSetIndex in range(elastixParameterSetDatabaseXml.GetNumberOfNestedElements()):
+      parameterSetXml = elastixParameterSetDatabaseXml.GetNestedElement(parameterSetIndex)
+      parameterFilesXml = parameterSetXml.FindNestedElementWithName('ParameterFiles')
+      parameterFiles = []
+      for parameterFileIndex in range(parameterFilesXml.GetNumberOfNestedElements()):
+        parameterFiles.append(parameterFilesXml.GetNestedElement(parameterFileIndex).GetAttribute('Name'))
+      self.registrationPresets.append([parameterSetXml.GetAttribute('id'), parameterSetXml.GetAttribute('modality'),
+        parameterSetXml.GetAttribute('content'), parameterSetXml.GetAttribute('description'), parameterSetXml.GetAttribute('publications'), parameterFiles])
+
+    return self.registrationPresets
 
   def startElastix(self, cmdLineArguments):
     self.addLog("Register volumes...")
@@ -545,7 +559,7 @@ class ElastixTest(ScriptedLoadableModuleTest):
     outputVolume.CreateDefaultDisplayNodes()
     
     logic = ElastixLogic()       
-    parameterFilenames = RegistrationPresets[0][RegistrationPresets_ParameterFilenames]
+    parameterFilenames = logic.getRegistrationPresets()[0][RegistrationPresets_ParameterFilenames]
     logic.registerVolumes(tumor1, tumor2, parameterFilenames, None, outputVolume)
     
     self.delayDisplay('Test passed!')
@@ -557,53 +571,3 @@ RegistrationPresets_Content = 2
 RegistrationPresets_Description = 3
 RegistrationPresets_Publications = 4
 RegistrationPresets_ParameterFilenames = 5
-
-RegistrationPresets = [
-[ "default0", "generic", "all", "'default' parameter files for starting elastix users", "", ["Parameters_Rigid.txt", "Parameters_BSpline.txt"]],
-[ "par0000", "3D MR T1, monomodal", "brain", "interpatient; affine + B-spline transformation; mutual information", "van der Lijn (2009) - Cerebellum Segmentation in MRI Using Atlas Registration and Local Multi-Scale Image Descriptors", ["Par0000affine.txt", "Par0000bspline.txt"]],
-[ "par0001", "3D MR BFFE, monomodal", "prostate", "interpatient; several transformation models; localised mutual information", "Klein & Staring (2010) - elastix: a toolbox for intensity-based medical image registration", [ "Par0001translation.txt", "Par0001bspline16.txt"]],
-[ "par0002", "3D MR T1 & PET", "brain", "intrapatient; translation; mutual information", "Klein & Staring (2010) - elastix: a toolbox for intensity-based medical image registration", ["Par0002.fs.MI.rigid.RandomCoordinate.txt"]],
-[ "par0003", "3D CT, monomodal", "lung", "interpatient; affine + B-spline transformation; mutual information", "Klein & Staring (2010) - elastix: a toolbox for intensity-based medical image registration", ["Par0003.affine.txt", "Par0003.bs-R4-fg.txt"]],
-#[ "par0004", "3D CT, monomodal", "lung", "intrapatient; rigid + B-spline transformation; mutual information", "Staring (2007) - A Rigidity Penalty Term for Nonrigid Registration"],
-#[ "par0005", "3D MR T2, monomodal", "cervix", "intrapatient; rigid + B-spline transformation; multi-feature &alpha;-mutual information", "Staring (2009) - Registration of Cervical MRI Using Multifeature Mutual Information"],
-#[ "par0006", "3D MR T2*, histology", "tumor", "rigid + B-spline transformation; multi-feature &alpha;-mutual information", "Alic (2011) - Facilitating tumor functional assessment by spatially relating 3D tumor histology and in vivo MRI: Image registration approach"],
-#[ "par0007", "4D CT, monomodal", "lung", "intrapatient; B-spline transformation; mutual information", "Ding (2010) - 4DCT-Based Measurement of Radiation Induced Changes in Pulmonary Function", ["Parameters.MI.Coarse.Bspline_tuned.txt", "Parameters.MI.Fine.Bspline_tuned.txt", "Parameters.MI.RP.Bspline_tuned.txt"]],
-[ "par0008", "3D CT, monomodal", "lung", "intrapatient; B-spline transformation; mutual information", "Murphy (2008) - Semi-Automatic Reference Standard Construction for Quantitative Evaluation of Lung CT Registration", ["Parameters.Par0008.affine.txt", "Parameters.Par0008.elastic.txt"]],
-[ "par0009", "3D MRI, monomodal", "brain", "interpatient; B-spline transformation; mutual information", "Artaechevarria (2009) - Combination strategies in multi-atlas image segmentation: application to brain MR data", ["Parameters.Par0009.affine.txt", "Parameters.Par0009.elastic.txt"]],
-[ "par0010", "3D MR HASTE, monomodal", "brain", "interpatient; affine + B-spline transformation; mutual information", "Klein (2010) - Early diagnosis of dementia based on intersubject whole-brain dissimilarities", ["Par0010affine.txt", "Par0010bspline.txt"]],
-[ "par0011", "3D CT, monomodal", "lung", "intrapatient (sometimes intra-sheep); B-spline transformation; normalized correlation", "Staring (2010), Pulmonary Image Registration With <tt>elastix</tt> Using a Standard Intensity-Based Algorithm", ["Parameters.Par0011.affine.txt", "Parameters.Par0011.bspline1_s.txt", "Parameters.Par0011.bspline2_s.txt"]],
-#[ "par0012", "4D CT(A), 3D MR, 3D US, 3D synthetic, monomodal", "heart, lung, carotid", "motion estimation; (cyclic) B-spline transformation; variance over last dimension", "Metz (2011), Nonrigid registration of dynamic medical imaging data using nD+t B-splines and a groupwise optimization approach"],
-[ "par0013", "2D x-ray - 3D CT ", "cerebral", "registration of interventional x-ray data to 3D CT for motion estimation, patient positioning or image guidance", "van der Bom (2011), Evaluation of optimization methods for intensity-based 2D-3D registration in x-ray guided interventions", ["par0013Powel_NGC_singleImage.txt"]],
-[ "par0014", "3D microCT", "whole-body mouse", "intra-mouse; B-spline transformation; combination of normalized correlation and the Euclidean distance metric", "Baiker (2011), Automated Registration of Whole-Body Follow-Up MicroCT Data of Mice", ["Parameters.similarity.txt", "Parameters.NCC.txt"]],
-[ "par0015", "3D CT, monomodal", "lung", "intrapatient; B-spline transformation; several similarity metrics", "Staring (2013), Towards Local Progression Estimation of Pulmonary Emphysema using CT", ["Parameters.Par0015.expA.patient.NC.affine.txt", "Parameters.Par0015.expA.patient.NC.bspline.txt" ]],
-#[ "par0016", "3D CT, monomodal", "lung", "intrapatient; Multi B-spline transformation; sliding motion", "Delmon (2011), Direction dependent B-splines decomposition for the registration of sliding objects"],
-[ "par0017", "3D MR, monomodal", "knee cartilage", "intrapatient; rigid; localized mutual information", "Bron (2013) -  Image registration improves human knee cartilage T1 mapping with delayed Gadolinium Enhanced MRI of Cartilage (dGEMRIC)", ["Par0017_cartilage_RigidLMI.txt"]],
-[ "par0018", "3D MR, multi sequence", "carotid artery", "interpatient; several transformation models; mutual information based similarity metrics", "van 't Klooster - Automated Registration of Multispectral MR Vessel Wall Images of the Carotid Artery", ["Par0018_3D_rigid_MI.txt", "Par0018_3D_affine_MI.txt", "Par0018_3D_bspline_MI_15.txt"]],
-[ "par0019", "3D CT, monomodal", "head and neck", "inter patient; rigid and B-spline transformation, advanced mean square and normalized correlation metrics", "Fortunati (2013) - Atlas based segmentation for hyperthermia treatment planning", ["Parameters_RigidAMS.txt", "Parameters_BSplineNCC.txt"]],
-[ "par0020", "3D MR, multi-contrast", "rat brain", "intra-subject, longitudinal data; rigid + affine + B-spline transformation, advanced normalized correlation metric with a transform bending energy penalty", "Khmelinskii (2013) - [http://proceedings.spiedigitallibrary.org/proceeding.aspx?articleid=1674633 A visualization platform for high-throughput, follow-up, co-registered multi-contrast MRI rat brain data. Mengler (2014) - [http://www.sciencedirect.com/science/article/pii/S1053811913008963 Brain maturation of the adolescent rat cortex and striatum: changes in volume and myelination]", ["Par0020rigid.txt", "Par0020affine.txt", "Par0020bspline2.txt" ]],
-#[ "par0021", "DT-MRI", "brain", "Registration optimisation for subject-FA images to FA template, multiple data sources", "De Groot (2013), Improving alignment in Tract-based spatial statistics: Evaluation and optimization of image registration"],
-#[ "par0022", "3D MR T2, monomodal", "cervix", "intrapatient; rigid + B-spline transformation; statistical shape regularization", "Berendsen (2013), Free-form image registration regularized by a statistical shape model: application to organ segmentation in cervical MR"],
-[ "par0023", "3D CT, 3D MR, multimodal", "head and neck", "intrapatient; rigid + B-spline transformation; localized mutual information combined with bending energy penalty", "Leibfarth (2013),  A strategy for multimodal deformable image registration to integrate PET/MR into radiotherapy treatment planning", ["Rigid.txt", "Deformable.txt"]],
-#[ "par0024", "3D MR T2, monomodal", "cervix", "interpatient; rigid + B-spline transformation; missing structure penalty", "Berendsen (2014), Registration of structurally dissimilar images in MRI-based brachytherapy"],
-[ "par0025", "3D MR, multi-contrast", "mouse brain", "intra-subject, longitudinal data; rigid + affine + B-spline transformation, advanced normalized correlation metric with a transform bending energy penalty|| Hammelrath (2015) - [http://www.sciencedirect.com/science/article/pii/S1053811915009039 Morphological maturation of the mouse brain: an ''in vivo'' MRI and histology investigation]", ["Par0025rigid.txt", "Par0025affine.txt", "Par0025bspline.txt"]],
-[ "par0026", "3D MR, multi-contrast", "rat brain", "intra-subject, longitudinal data; rigid + affine + B-spline transformation", " Hammelrath (2016)", ["Par0026rigid.txt", "Par0026affine.txt", "Par0026bspline.txt"]],
-#[ "par0027", "3D CT, 3D MR, multimodal, multi sequence", "head and neck", "intra patient; rigid + B-spline transformation; mutual information, multi parametric mutual information", "Fortunati (2014),  Feasibility of Multimodal Deformable Registration for Head and Neck Tumor Treatment Planning"],
-#[ "par0028", "3D CT, monomodal", "head and neck", "intra patient; rigid + B-spline transformation; several B-pline knot spacings; synthesized head and neck phantoms", "Brouwer (2014),  The effects of CT image characteristics and knot spacing on the spatial accuracy of B-spline deformable image registration in the head and neck geometry"],
-#[ "par0029", "3D diffusion-weighted MR images (DW-MRI)", "abdomen", "intra patient; B-spline transformation; mutual information", "Guyader (2014) - Influence of image registration on apparent diffusion coefficient images computed from free-breathing diffusion MR images of the abdomen"],
-[ "par0030", "3D T1 fat-suppressed Ga-enhanced extremity MR", "wrist", "inter patient; Affine + B-spline transformation; normalized cross correlation. intra patient; Rigid transformation; mutual information", "Roex - MSc thesis: Early Detection of Rheumatoid Arthritis using extremity MRI: Quantification of Bone Marrow Edema in the Carpal bones", ["Parameters0030_Euler.txt", "Parameters0030_Affine.txt", "Parameters0030_Bspline.txt"]],
-#[ "par0031", "3D CT, monomodal", "thorax", "intrapatient; regional-independent B-spline transformation; sliding surface penalty", "Berendsen (2014), Registration of organs with sliding interfaces and changing topologies"],
-[ "par0032", "3D DCE-MRI", "breast", "intrapatient; rigid + B-spline transformation; mutual information", "Gubern-Merida (2015), Automated localization of breast cancer in DCE-MRI", ["Par0032_rigid.txt", "Par0032_bsplines.txt"]],
-[ "par0033", "3D MR", "mouse brain", "inter-subject; rigid + affine + B-spline transformation; mutual information metric with a transform bending energy penalty", "Bink (2016), Kogelman (2016)", ["Par0033rigid.txt", "Par0033similarity.txt", "Par0033bspline.txt"]],
-[ "par0034", "2D CBF MR", "mouse brain", "inter-subject; rigid + affine + B-spline transformation; advanced normalized correlation + mutual information metric with a transform bending energy penalty", "Bink (2016)", ["Par0034rigid.txt", "Par0034affine.txt", "Par0034bspline.txt"]],
-#[ "par0035", "CT, MR, ultrasound", "brain, lung, abdomen", "intra-subject, inter-subject; mono-modal and multi-modal; rigid, affine and B-spline transformations; mean square difference, normalized correlation, mutual information", "Qiao (2015), Fast Automatic Step Size Estimation for Gradient Descent Optimization of Image Registration"],
-#[ "par0036", "MRI joint histograms", "whole-body", "intra-subject, inter-station; affine; normalized correlation", "Dzyubachyk (2015)"],
-#[ "par0037", "DTS", "lung", "intra-subject; translational registration; mutual information", "You (2015)"],
-#[ "par0038", "MRI", "mouse brain stroke", "intra-subject, longitudinal data; rigid + affine + B-spline transformation, advanced normalized correlation metric with a transform bending energy penalty", "Mulder, Khmelinskii, Dzyubachyk (2016)"],
-#[ "par0039", "quantitative MRI", "porcine heart, abdomen, carotid, brain", "intra-subject; affine or B-spline transformation; groupwise registration; PCA metrics", "Huizinga (2016), PCA-based groupwise image registration for quantitative MRI"],
-[ "par0040", "extremity MRI", "wrist", "intra-subject; affine transformation, mutual information metric", "Aizenberg (2016)", ["Par0040affine.txt"]],
-[ "par0041", "extremity MRI", "wrist", "inter-subject; similarity + B-spline transformation, advanced normalized correlation metric", "Aizenberg (2016)", ["Par0041similarity.txt", "Par0041bspline.txt"]],
-[ "par0042", "MRI", "spine", "inter-subject; rigid + B-spline transformation, advanced normalized correlation metric", "Aizenberg (2016)", ["Par0042rigid.txt", "Par0042bspline.txt"]],
-[ "par0043", "CT/ MR-based pseudo-CT", "pelvis (prostate)", "intra-subject; multi-resolution (4), rigid + B-spline transformation, Mutual Information metric (Mattes) with Adaptive Stochastic Gradient Descent optimizer", "Maspero M, et al. (2017), [http://iopscience.iop.org/article/10.1088/1361-6560/aa4fe7 Quantification of confounding factors in MRI-based dose calculations as applied to prostate IMRT.] Phys Med Biol. 62(3)", ["Par0043rigid.txt"]],
-[ "par0044", "3D CT, multi-contrast ", "cardiac", "intra-subject; multi-image; affine (Mutual Information) + multi-image B-spline transformation (Mutual Information and mean square difference); ", "Giordano (2016)", ["Par0044Affine.txt", "Par0044NonRigid.txt"]],
-#[ "par0045", " MRI", "mouse brain", "inter and intra-subject, perfusion data; rigid + affine + B-spline transformation; ", "Hirschler and Munting (2016)"]
-]
